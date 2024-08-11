8eabdadbb9e1945c5ef7a55da89cfa20edd7161a
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-08-11 15:50:24 +0200

    text => seq

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@ impl<T: ptr_hash::KeyT> Phf<T> for ptr_hash::PtrHash<T> {
 
 pub trait Minimizer {
     fn minimizer_one(&self, window: impl IntoBpIterator) -> (usize, u64); // (pos, value)
-    fn minimizers(&self, text: impl IntoBpIterator) -> impl Iterator<Item = (usize, u64)>; // (pos, value)
+    fn minimizers(&self, seq: impl IntoBpIterator) -> impl Iterator<Item = (usize, u64)>; // (pos, value)
     fn k(&self) -> usize;
     fn w(&self) -> usize;
     fn l(&self) -> usize {
@@ -142,10 +142,10 @@ impl Minimizer for NtMinimizer {
         (pos, val)
     }
 
-    fn minimizers(&self, text: impl IntoBpIterator) -> impl Iterator<Item = (usize, u64)> {
-        minimizers::par::minimizer::minimizer_scalar_it::<false>(text, self.k, self.w).map(
+    fn minimizers(&self, seq: impl IntoBpIterator) -> impl Iterator<Item = (usize, u64)> {
+        minimizers::par::minimizer::minimizer_scalar_it::<false>(seq, self.k, self.w).map(
             move |pos| {
-                let val = text.sub_slice(pos as usize, self.k).to_word() as u64;
+                let val = seq.sub_slice(pos as usize, self.k).to_word() as u64;
                 (pos as usize, val)
             },
         )
@@ -165,7 +165,7 @@ impl Minimizer for NtMinimizer {
 // TODO: query throughput benchmarks
 pub struct SsHash<H: Phf<u64>, M: Minimizer, P: BpStorage> {
     minimizer: M,
-    text: P,               // Todo bitpacked
+    seqs: P,               // Todo bitpacked
     endpoints: Vec<usize>, // Todo EF
     num_uniq_minis: usize,
     phf: H,
@@ -175,26 +175,26 @@ pub struct SsHash<H: Phf<u64>, M: Minimizer, P: BpStorage> {
 
 impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
     // Convenience wrapper around build_from_slice that doesn't require type annotations.
-    pub fn build(text: &[&P], minimizer: M, phf_builder: impl PhfBuilder<u64, Phf = H>) -> Self {
-        let text = text.iter().map(|x| x.get()).collect_vec();
-        Self::build_from_slice(&text, minimizer, phf_builder)
+    pub fn build(seq: &[&P], minimizer: M, phf_builder: impl PhfBuilder<u64, Phf = H>) -> Self {
+        let seq = seq.iter().map(|x| x.get()).collect_vec();
+        Self::build_from_slice(&seq, minimizer, phf_builder)
     }
     pub fn build_from_slice(
-        text: &[P::BpSlice<'_>],
+        seqs: &[P::BpSlice<'_>],
         minimizer: M,
         phf_builder: impl PhfBuilder<u64, Phf = H>,
     ) -> Self {
-        let endpoints = text
+        let endpoints = seqs
             .iter()
             .scan(0, |acc, x| {
                 *acc += x.len().next_multiple_of(4);
                 Some(*acc)
             })
             .collect_vec();
-        let text = P::concat(text);
+        let seqs = P::concat(seqs);
 
         // Todo split at input seq boundaries
-        let mut minis = minimizer.minimizers(text.get()).dedup().collect_vec();
+        let mut minis = minimizer.minimizers(seqs.get()).dedup().collect_vec();
         minis.sort_by_key(|x| x.1);
         // Todo: is passing an iterator to the phf builder sufficient?
         let uniq_mini_vals = minis.iter().map(|x| x.1).dedup().collect_vec();
@@ -220,7 +220,7 @@ impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
             sizes_ef.push(size);
         }
 
-        let offset_bits = text.get().len().ilog2() as usize + 1;
+        let offset_bits = seqs.get().len().ilog2() as usize + 1;
         let mut offsets = bit_field_vec![offset_bits; minis.len(); 0];
         for (mini_val, chunk) in minis.iter().chunk_by(|x| x.1).into_iter() {
             if let Some(hash) = phf.hash(mini_val) {
@@ -236,7 +236,7 @@ impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
 
         Self {
             minimizer,
-            text,
+            seqs,
             endpoints,
             num_uniq_minis,
             phf,
@@ -257,10 +257,10 @@ impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
                 continue;
             }
             let lmer_pos = offset as usize - minimizer_pos;
-            if lmer_pos + self.minimizer.l() > self.text.get().len() {
+            if lmer_pos + self.minimizer.l() > self.seqs.get().len() {
                 continue;
             }
-            let lmer = &self.text.slice(lmer_pos..lmer_pos + self.minimizer.l());
+            let lmer = &self.seqs.slice(lmer_pos..lmer_pos + self.minimizer.l());
             if lmer.to_word() == window.to_word() {
                 return Some((
                     lmer_pos,
@@ -283,15 +283,15 @@ impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
         use size::Size;
         use std::mem::size_of_val;
         // All sizes in bytes.
-        let text = self.text.size();
+        let seqs = self.seqs.size();
         let endpoints = size_of_val(self.endpoints.as_slice());
         let phf = self.phf.size();
         // TODO: Include size of rank-select structures.
         let sizes = EliasFano::<(), ()>::estimate_size(self.offsets.len(), self.sizes.len()) / 8;
         let offsets = self.offsets.len() * self.offsets.bit_width() / 8;
-        let total = text + endpoints + phf + sizes + offsets;
+        let total = seqs + endpoints + phf + sizes + offsets;
 
-        let num_bp = self.text.get().len() as f32;
+        let num_bp = self.seqs.get().len() as f32;
         let num_minis = self.offsets.len() as f32;
 
         eprintln!(
@@ -299,13 +299,13 @@ impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
             "len", "max", "memory", "bits/kmer", "bits/mini", "bits/uniq mini"
         );
         eprintln!(
-            "text:      {:>10} {:>10} {:>10} {:>10.1} {:>10.1} {:>10.1}",
-            self.text.get().len(),
+            "seqs:      {:>10} {:>10} {:>10} {:>10.1} {:>10.1} {:>10.1}",
+            self.seqs.get().len(),
             3,
-            format!("{}", Size::from_bytes(text)),
-            8. * text as f32 / num_bp,
-            8. * text as f32 / num_minis,
-            8. * text as f32 / self.num_uniq_minis as f32
+            format!("{}", Size::from_bytes(seqs)),
+            8. * seqs as f32 / num_bp,
+            8. * seqs as f32 / num_minis,
+            8. * seqs as f32 / self.num_uniq_minis as f32
         );
         eprintln!(
             "endpoints: {:>10} {:>10} {:>10} {:>10.1} {:>10.1} {:>10.1}",
@@ -337,7 +337,7 @@ impl<H: Phf<u64>, M: Minimizer, P: BpStorage> SsHash<H, M, P> {
         eprintln!(
             "offsets:   {:>10} {:>10} {:>10} {:>10.1} {:>10.1} {:>10.1}",
             self.offsets.len(),
-            self.text.get().len(),
+            self.seqs.get().len(),
             format!("{}", Size::from_bytes(offsets)),
             8. * offsets as f32 / num_bp,
             8. * offsets as f32 / num_minis,
@@ -369,13 +369,13 @@ mod test {
         alphabet: usize,
     ) {
         let n = 100;
-        let text = P::random(n, alphabet);
+        let seq = P::random(n, alphabet);
         let l = minimizer.k() + minimizer.w() - 1;
-        let sshash = SsHash::build(&[&text], minimizer, phf_builder);
+        let sshash = SsHash::build(&[&seq], minimizer, phf_builder);
 
         // The alphabet is large enough that we assume no duplicate k-mers occur.
-        for i in 0..text.get().len() - l {
-            let window = text.slice(i..i + l);
+        for i in 0..seq.get().len() - l {
+            let window = seq.slice(i..i + l);
             assert_eq!(sshash.query_one(window), Some((i, 0)));
         }
     }
@@ -386,12 +386,12 @@ mod test {
         alphabet: usize,
     ) {
         let n = 10000;
-        let text = P::random(n, alphabet);
+        let seq = P::random(n, alphabet);
         let l = minimizer.l();
-        let sshash = SsHash::build(&[&text], minimizer, phf_builder);
+        let sshash = SsHash::build(&[&seq], minimizer, phf_builder);
 
         // The alphabet is large enough that we assume no duplicate k-mers occur.
-        for _ in 0..text.get().len() {
+        for _ in 0..seq.get().len() {
             let window = P::random(l, alphabet);
             assert_eq!(sshash.query_one(window.get()), None);
         }
@@ -468,7 +468,7 @@ mod test {
     #[ignore]
     #[test]
     fn print_size() {
-        let text = PackedVec::random(10000000, 4);
+        let seq = PackedVec::random(10000000, 4);
         let k = 21;
         let w = 11;
         let minimizer = NtMinimizer { k, w };
@@ -477,7 +477,7 @@ mod test {
             ..Default::default()
         };
 
-        let sshash = SsHash::build(&[&text], minimizer, phf_builder);
+        let sshash = SsHash::build(&[&seq], minimizer, phf_builder);
         sshash.print_size();
     }
 
@@ -486,7 +486,7 @@ mod test {
     fn print_size_hg() {
         eprintln!("Reading..");
         let start = std::time::Instant::now();
-        let mut packed_text = vec![];
+        let mut packed_seq = vec![];
         let Ok(mut reader) = needletail::parse_fastx_file("human-genome.fa") else {
             panic!("Did not find human-genome.fa. Add/symlink it to test runtime on it.");
         };
@@ -497,14 +497,14 @@ mod test {
                 std::str::from_utf8(r.id()),
                 r.raw_seq().len()
             );
-            pack(&r.raw_seq(), &mut packed_text);
-            eprintln!("Packed len {:?}", packed_text.len());
+            pack(&r.raw_seq(), &mut packed_seq);
+            eprintln!("Packed len {:?}", packed_seq.len());
         }
         eprintln!("Packing took {:?}", start.elapsed());
 
-        let text = PackedVec {
-            len: packed_text.len() * 4,
-            seq: packed_text,
+        let seq = PackedVec {
+            len: packed_seq.len() * 4,
+            seq: packed_seq,
         };
         let k = 20;
         let w = 12;
@@ -514,14 +514,14 @@ mod test {
             ..Default::default()
         };
 
-        let sshash = SsHash::build(&[&text], minimizer, phf_builder);
+        let sshash = SsHash::build(&[&seq], minimizer, phf_builder);
         sshash.print_size();
     }
 
-    fn pack(text: &[u8], packed: &mut Vec<u8>) {
+    fn pack(seq: &[u8], packed: &mut Vec<u8>) {
         let mut packed_byte = 0;
         let mut packed_len = 0;
-        for &base in text {
+        for &base in seq {
             packed_byte |= match base {
                 b'a' | b'A' => 0,
                 b'c' | b'C' => 1,
